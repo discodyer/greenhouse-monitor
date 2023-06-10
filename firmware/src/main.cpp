4a9ae84cda8da0031eaa6e5504b3209bcb35b001
discodyer <cody23333@gmail.com>
2023-06-10 15:11:43 +0800

    可以同步网络时间

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -1,18 +1,88 @@
 #include <Arduino.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#include <WiFi.h>
 
-// put function declarations here:
-int myFunction(int, int);
+void displayLocalTime();
+
+const char *ssid = "HUAWEI-AE87"; //你的网络名称
+const char *password = "uavroom114514"; //你的网络密码
+const char *ntpServer = "ntp1.aliyun.com";
+const long gmtOffset_sec = 8 * 3600; //东八区时区偏移量
+const int daylightOffset_sec = 0; //夏令时偏移量
+
+
+// 定义OLED屏幕的宽度和高度
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+
+#define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
+#define SCREEN_ADDRESS 0x3C
+
+// 设置OLED的IIC引脚
+#define OLED_SDA    21
+#define OLED_SCL    22
+
+// 创建一个OLED对象
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+    // 初始化串口通信
+    Serial.begin(115200);
+
+    // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
+    if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
+        Serial.println(F("SSD1306 allocation failed"));
+        for(;;); // Don't proceed, loop forever
+    }
+
+    // Show initial display buffer contents on the screen --
+    // the library initializes this with an Adafruit splash screen.
+    display.display();
+    delay(2000); // Pause for 2 seconds
+
+    // Clear the buffer
+    display.clearDisplay();
+    // Draw a single pixel in white
+    display.drawPixel(10, 10, SSD1306_WHITE);
+    // Show the display buffer on the screen. You MUST call display() after
+    // drawing commands to make them visible on screen!
+    display.display();
+    delay(100);
+
+    // 连接wifi
+    WiFi.begin(ssid, password);
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+    }
+    Serial.println("WiFi connected!");
+    // 从网络时间服务器上获取并设置时间
+    // 获取成功后芯片会使用RTC时钟保持时间的更新
+    configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
+    displayLocalTime();
+
+
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+    displayLocalTime();
+    delay(1000);
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void displayLocalTime() {
+    struct tm timeinfo;
+    if (!getLocalTime(&timeinfo)) {
+        Serial.println("Failed to obtain time");
+        return;
+    }
+    Serial.println(&timeinfo, "%F %T %A"); // 格式化输出
+    display.setTextSize(1);
+    display.clearDisplay();
+    display.setTextColor(WHITE);
+    display.setCursor(0, 0);
+    // Display static text
+    display.println(&timeinfo, "%F %T\n%A");
+    display.display();
 }
